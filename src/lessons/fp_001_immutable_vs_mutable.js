3b1c05a706b919710c8ecde7366fd9f29c25eac1
Alain Horner <alain.hornair@gmail.com>
2021-04-26 11:02:11 +0200

    lesson 1: documenting directly inside the code

--- src/lessons/fp_001_immutable_vs_mutable.js
+++ src/lessons/fp_001_immutable_vs_mutable.js
@@ -1,3 +1,15 @@
+/**
+ * Immutable: something can't be modified after it was created
+ * Reassignment !== Mutability (const doesn't allow reassignment, but that doesn't mean that a const is immutable)
+ */
+
+const part = 'someString'.slice(0, 2); // (immutable operation: slice doesn't change the original string)
+console.log(part);
+
+/**
+ * Changing a property of an object is a mutable operation
+ * Prevent mutating an object: Object.freeze(myObj)
+ */
 const originalObj = { id: '🍟', name: 'Jumbo Fries', price: 199 };
 
 originalObj.id = '😎'; // everything fine
